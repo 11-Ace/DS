a27598e8d3f4e93be52cc0acdb51dd717a46b12f
Odapu Richard <117220948+11-Ace@users.noreply.github.com>
2024-12-03 14:02:42 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+});
