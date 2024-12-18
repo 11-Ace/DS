6ae4064d7b988a76bb439e4d729fa24e22b8525f
Odapu Richard <117220948+11-Ace@users.noreply.github.com>
2024-12-18 17:23:42 +0100

    font changed, header fixed position to top, skype icon changed to whatsapp icon, slider speed and style changed

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
+const header = document.querySelector('header');
+
+// Slider
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
   loop: true,
   autoplay: {
-    delay: 2000,
+    delay: 4000,
   },
-  effect: 'fade',
-  fadeEffect: {
-    crossFade: true,
+  // grabCursor: true,
+  effect: 'creative',
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: [0, 0, -400],
+    },
+    next: {
+      translate: ['100%', 0, 0],
+    },
   },
 });
+
+// Offset contents after fixed header
+const offsetHeader = document.getElementById('offset-header');
+offsetHeader.style.height = `${header.offsetHeight}px`;
+
+// Change color of header on page scroll
+/* window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    header.classList.add('header-color');
+  } else {
+    header.classList.remove('header-color');
+  }
+}); */
